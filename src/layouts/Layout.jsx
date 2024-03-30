d90d25040e2a6a50fac6870ccf7f6fb9a550453d
hossein <hosseinbakhtiari781@outlook.com>
2024-03-30 09:20:29 +0330

    create Layout file and the header and footer were styled

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import styles from "./Layout.module.css"
 function Layout({children}) {
   return (
     <>
-        <header>header</header>
+        <header className={styles.header}>
+            <h1>Book App</h1>
+            <p><a href="https://github.com/Hossein-Bakhtiari/rjs-book-app-files-main">GitHub</a> | React.js Project </p>
+        </header>
         {children}
-        <footer>footer</footer>
+        <footer className={styles.footer}>Developed by Hossein</footer>
     </>
   )
 }
